b794b17880cae7252c582f08cf84e0f797af981d
amyxie361 <amyxie361@outlook.com>
2022-11-26 00:45:14 +0000

    save local changes

--- src/train.sh
+++ src/train.sh
@@ -1,23 +1,22 @@
-for bs in 64
+for bs in 16 8
 do
-for lr in 1e-5
+for lr in 1e-5 3e-5 3e-6
 do
 CUDA_VISIBLE_DEVICES=2 python -m tevatron.driver.train \
   --output_dir model_scifact_xyq_bs${bs}_lr${lr} \
   --model_name_or_path facebook/dpr-question_encoder-single-nq-base \
   --save_steps 2000 \
-  --dataset_name Tevatron/scifact \
+  --dataset_name Tevatron/msmarco-passage \
   --fp16 \
   --per_device_train_batch_size ${bs} \
-  --train_n_passages 2 \
+  --train_n_passages 8 \
   --learning_rate ${lr} \
-  --q_max_len 64 \
-  --grad_cache \
-  --p_max_len 512 \
-  --num_train_epochs 50 \
-  --gc_p_chunk_size 8 \
-  --logging_steps 10 \
+  --q_max_len 32 \
+  --p_max_len 256 \
+  --num_train_epochs 10 \
+  --logging_steps 500 \
   --overwrite_output_dir
 
 done
 done
+
